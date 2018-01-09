6b62891ea4c4f3ee77cb3d2afc334a2de4a78b13
rayouyou16 <rayouyou16@hotmail.fr>
2018-01-09 20:57:38 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,6 +1,12 @@
 const Discord = require('discord.js')
 const bot = new Discord.Client()
 
+bot.on("message", (message) => {
+    if(message.content === ".Waifu Nara") {
+      message.channel.send("Nya");
+    }
+  });
+
 bot.on('ready', function() {
     bot.user.setGame("créé des commandes");
     Console.log('Connecté')
