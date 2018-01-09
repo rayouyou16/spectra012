900345a6c0b77b1fc4586020a9d9bf7cf553d87f
rayouyou16 <rayouyou16@hotmail.fr>
2018-01-09 21:01:15 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js')
 const bot = new Discord.Client()
 
 bot.on("message", (message) => {
-    if(message.content === ".WaifuNara") {
+    if(message.content === "%Waifuu") {
       message.channel.send("Nya");
     }
   });
